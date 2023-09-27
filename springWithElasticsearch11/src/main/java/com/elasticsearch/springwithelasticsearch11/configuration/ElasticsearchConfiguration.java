b636f56535488b5e03a80d9a8ac0372f8bc79490
JEONSEUNGREE <jws08291@naver.com>
2023-09-27 10:41:47 +0900

    fuzzyQuery

---
+++ springWithElasticsearch11/src/main/java/com/elasticsearch/springwithelasticsearch11/configuration/ElasticsearchConfiguration.java
@@ -0,0 +1,30 @@
+package com.elasticsearch.springwithelasticsearch11.configuration;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ElasticsearchConfiguration {
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        RestClient restClient = RestClient.builder(
+                new HttpHost("localhost", 9200)).build();
+
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        // And create the API client
+        ElasticsearchClient client = new ElasticsearchClient(transport);
+
+        return client;
+    }
+}
